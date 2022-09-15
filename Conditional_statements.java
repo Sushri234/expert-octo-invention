a0196c7e9ca585bc765a0bcfe85d7a50c274c2a2
sushri234 <sushrisangita82@gmail.com>
2022-09-15 17:31:13 +0530

    Conditional_statements.java

---
+++ Conditional_statements.java
@@ -0,0 +1,60 @@
+package Automation;
+
+public class Conditional_statements {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//IF STATEMENT
+		int age=27;
+		if (age>18) {
+			System.out.println("yes you can marraige");
+		}
+		else {
+			System.out.println("no girl not yet");
+		}
+		
+		//FOR LOOP
+		for( int i=1;i<=10;i++) {
+			System.out.println(i+"");
+		}
+		//WHILE LOOP
+		int a= 17;
+		while(a>=1) {
+			System.out.println("hello");
+			a--;
+		}
+		//DO WHILE LOOP
+		int n=12;
+		do {
+			System.out.println(n);
+			n++;
+		}
+		while(n<7);
+		
+		//switch STATEMENT
+		int a1=1,b=2,ch;
+		switch(a1+b)
+		{
+		case 1:
+		System.out.println("hii");
+		break;
+		case 2:
+		System.out.println("bye");
+		break;
+		case 3:
+		System.out.println("tie");
+		break ;
+		default:
+	
+			
+		}
+		
+			
+			
+		}
+		
+		
+
+	}
+
+
